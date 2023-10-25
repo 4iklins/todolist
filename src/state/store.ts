c2ab36717426660be05554809bc3962b164eb8b5
4iklins <4iklins@gmail.com>
2023-10-25 23:31:03 +0300

    store redux ext

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,25 @@
-import { legacy_createStore, combineReducers } from 'redux';
+//
 import { tasksReducer } from './tasks-reducer';
 import { todolistsReducer } from './todolists-reducer';
+import { combineReducers, compose, legacy_createStore } from 'redux';
 
-const reducers = combineReducers({
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+// объединяя reducer-ы с помощью combineReducers,
+// мы задаём структуру нашего единственного объекта-состояния
+const rootReducer = combineReducers({
   tasks: tasksReducer,
   todolists: todolistsReducer,
 });
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+// непосредственно создаём store
+export const store = legacy_createStore(rootReducer, composeEnhancers());
+// определить автоматически тип всего объекта состояния
+export type StateType = ReturnType<typeof rootReducer>;
 
-export const store = legacy_createStore(reducers);
-
-export type Dispatch = typeof store.dispatch
-export type StateType = ReturnType<typeof reducers>
-
-//@ts-ignore
-window.store = store
+// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
+// @ts-ignore
+window.store = store;
