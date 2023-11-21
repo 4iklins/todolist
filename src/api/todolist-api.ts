1101a4b701d2327eb78517dbc71495048bb44b72
4iklins <4iklins@gmail.com>
2023-11-21 17:24:06 +0300

    rest api

--- src/api/todolist-api.ts
+++ src/api/todolist-api.ts
@@ -15,6 +15,18 @@ export const todolistApi = {
   deleteTodo(todolistId: string) {
     return instance.delete<ResponseType>(`/todo-lists/${todolistId}`);
   },
+  getTasks(todolistId: string) {
+    return instance.get<GetTasksResponseType>(`/todo-lists/${todolistId}/tasks`);
+  },
+  createTask(todolistId: string, title: string) {
+    return instance.post<ResponseType<TaskType>>(`/todo-lists/${todolistId}/tasks`, { title });
+  },
+  updateTask(todolistId: string, taskId: string, task: TaskUpdateType) {
+    return instance.put<ResponseType<TaskType>>(`/todo-lists/${todolistId}/tasks/${taskId}`, { ...task });
+  },
+  deleteTask(todolistId: string, taskId: string) {
+    return instance.delete<ResponseType>(`/todo-lists/${todolistId}/tasks/${taskId}`);
+  },
 };
 type TodolistType = {
   id: string;
@@ -23,10 +35,41 @@ type TodolistType = {
   order: number;
 };
 
+type TaskType = {
+  description: string;
+  title: string;
+  completed: boolean;
+  status: number;
+  priority: number;
+  startDate: Date;
+  deadline: Date;
+  id: string;
+  todoListId: string;
+  order: number;
+  addedDate: Date;
+};
+
+export type TaskUpdateType = {
+  title: string;
+  description: string;
+  completed: boolean;
+  status: number;
+  priority: number;
+  startDate: Date;
+  deadline: Date;
+};
+
 export type ResponseType<T = {}> = {
   resultCode: number;
   messages: string[];
+  fieldsError: string[];
   data: {
     item: T;
   };
+};
+
+type GetTasksResponseType = {
+  items: TaskType[];
+  totalCount: number;
+  error: string;
 };
