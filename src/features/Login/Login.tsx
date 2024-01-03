188f4af32e79c426bc13c9d3491b0526e3ba346b
4iklins <4iklins@gmail.com>
2024-01-03 18:02:56 +0300

    formdata/validation

--- src/features/Login/Login.tsx
+++ src/features/Login/Login.tsx
@@ -7,32 +7,91 @@ import FormGroup from '@mui/material/FormGroup';
 import FormLabel from '@mui/material/FormLabel';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useFormik } from 'formik';
+import { Link } from '@mui/material';
 
+type FormDataType = {
+  email: string;
+  password: string;
+  rememberMe: boolean;
+};
+
+const EMAIL_REGEXP = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 export const Login = () => {
+  const { values, handleSubmit, handleChange, errors, touched, getFieldProps, resetForm } = useFormik<FormDataType>({
+    initialValues: {
+      email: '',
+      password: '',
+      rememberMe: false,
+    },
+    onSubmit: values => {
+      alert(JSON.stringify(values));
+      resetForm();
+    },
+    validate: values => {
+      const errors: Partial<FormDataType> = {};
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (!EMAIL_REGEXP.test(values.email)) {
+        errors.email = 'Invalid email address';
+      }
+      if (!values.password) {
+        errors.password = 'Required';
+      } else if (values.password.length <= 3) {
+        errors.password = 'Password must be more than 3 characters';
+      }
+      return errors;
+    },
+  });
   return (
     <Grid container justifyContent={'center'}>
       <Grid item justifyContent={'center'}>
-        <FormControl>
-          <FormLabel>
-            <p>
-              To log in get registered
-              <a href={'https://social-network.samuraijs.com/'} target={'_blank'}>
-                here
-              </a>
-            </p>
-            <p>or use common test account credentials:</p>
-            <p>Email: free@samuraijs.com</p>
-            <p>Password: free</p>
-          </FormLabel>
-          <FormGroup>
-            <TextField label='Email' margin='normal' />
-            <TextField type='password' label='Password' margin='normal' />
-            <FormControlLabel label={'Remember me'} control={<Checkbox />} />
-            <Button type={'submit'} variant={'contained'} color={'primary'}>
-              Login
-            </Button>
-          </FormGroup>
-        </FormControl>
+        <form onSubmit={handleSubmit}>
+          <FormControl>
+            <FormLabel>
+              <p>
+                To log in get registered
+                <Link
+                  href={'https://social-network.samuraijs.com/'}
+                  target={'_blank'}
+                  sx={{ marginLeft: '5px' }}
+                  underline='hover'>
+                  here
+                </Link>
+              </p>
+              <p>or use common test account credentials:</p>
+              <p>Email: free@samuraijs.com</p>
+              <p>Password: free</p>
+            </FormLabel>
+            <FormGroup>
+              <TextField
+                size='small'
+                label='Email'
+                {...getFieldProps('email')}
+                error={touched.email && !!errors.email}
+                helperText={touched.email && errors.email}
+                type='email'
+                sx={{ margin: 0, height: '64px' }}
+              />
+              <TextField
+                size='small'
+                type='password'
+                label='Password'
+                {...getFieldProps('password')}
+                error={touched.password && !!errors.password}
+                helperText={touched.password && errors.password}
+                sx={{ margin: 0, height: '64px' }}
+              />
+              <FormControlLabel
+                label={'Remember me'}
+                control={<Checkbox name='rememberMe' checked={values.rememberMe} onChange={handleChange} />}
+              />
+              <Button type={'submit'} variant={'contained'} color={'primary'} sx={{ margin: '24px 0' }}>
+                Login
+              </Button>
+            </FormGroup>
+          </FormControl>
+        </form>
       </Grid>
     </Grid>
   );
